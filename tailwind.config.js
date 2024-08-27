ff4215dd5dbf61e4cbbbcc5c1a4347913cee1873
slowburn-404 <borisochola32@gmail.com>
2024-08-27 12:57:38 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
